5e6716de315b40dcab8a401ff3294f8299077c67
kureedoow <rapidmaan1@gmail.com>
2025-04-25 16:43:53 +0600

    tailwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>this is my first project</div>;
+  return (
+    <div className="bg-black text-2xl text-white text-center">
+      This is my first project set up in react
+    </div>
+  );
 };
 
 export default App;
